7dfa0236a8408a22f85771ec2b92b29d0675d5a3
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-10 18:01:46 +0530

    Output-Aliasing-Passing-Event-Data-Object-Interface

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,14 +8,16 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class FavoriteComponent implements OnInit {
   // [is-favorite] Aliasing Input name
   @Input('is-favorite') isFavorite: boolean;
-  @Output() change = new EventEmitter();
+
+  // [change] Aliasing Input name
+  @Output('change') click = new EventEmitter();
   public isBorder = true;
   constructor() { }
 
   bgChange() {
     this.isFavorite = !this.isFavorite;
     this.isBorder = !this.isBorder;
-    this.change.emit({newValue: this.isFavorite});
+    this.click.emit({newValue: this.isFavorite});
   }
   ngOnInit() {
   }
