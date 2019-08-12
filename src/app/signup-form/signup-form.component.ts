2470d499f4f9919bb23ec5de2241433e676d7dfc
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-12 17:11:52 +0530

    Reactive-Form-Adding-Validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -9,9 +9,13 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class SignupFormComponent implements OnInit {
 
   public form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
+
+  get username() {
+    return this.form.get('username');
+  }
 
   constructor() { }
 
