ce6637ab10a977549da9d557869938edde6439f3
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-12 18:07:36 +0530

    Reactive-Form-Implementing-Custom-Validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -4,7 +4,6 @@ export class UsernameValidators {
     static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
         if((control.value as string).indexOf('') >= 0) {
             return { cannotContainSpace: true };
-        } else {
             return null;
         }
     }
